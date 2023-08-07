98dae3fdb0000f0ead97f902db5d77c59291c6fc
chessea <franco.rojas.leon.mt@gmail.com>
2023-08-07 14:23:35 -0400

    first

---
+++ src/__init__.py
@@ -0,0 +1,69 @@
+
+from flask import Flask, redirect, jsonify
+from dotenv import load_dotenv
+import os
+from src.auth import auth
+from src.bookmarks import bookmarks
+from src.database import db, Bookmark
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+from src.constants.http_status_code import *
+from flasgger import Swagger, swag_from
+from src.config.swagger import template, swagger_config
+
+
+# Cargar variables de entorno()
+load_dotenv()
+
+
+def create_app( test_config=None):
+ 
+
+    app = Flask( __name__, 
+                instance_relative_config=True)
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
+            JWT_ACCESS_TOKEN_EXPIRES=timedelta(days=int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES'))),
+            SWAGGER={
+                'title': "Bookmarks API",
+                'Universion': 3
+            }
+        )
+    else:
+        app.config.from_mapping(test_config)
+    
+    db.app=app
+    db.init_app(app)
+
+    JWTManager(app)
+
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
+
+
+    Swagger(app,config=swagger_config, template=template)
+
+
+    @app.get('/<short_url>')
+    @swag_from('./docs/short_url.yaml')
+    def redirect_to_url(short_url):
+        bookmark=Bookmark.query.filter_by(short_url=short_url).first_or_404()
+
+        if bookmark:
+            bookmark.visits = bookmark.visits+1  
+            db.session.commit() 
+            return redirect(bookmark.url)
+    
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({'error': 'Not found'}), HTTP_404_NOT_FOUND
+
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
+
+    return app
